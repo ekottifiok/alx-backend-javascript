a18e85f44f4aa673cc5cb359163153392336253f
Ifiok Ekott <ekottifiok@gmail.com>
2023-05-22 15:01:35 +0000

    fixed eslint error in 3-read_file_async

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -3,14 +3,15 @@ const fs = require('fs');
 const countStudents = (fileName) => new Promise((resolve, reject) => {
   fs.readFile(fileName, 'utf-8', (err, data) => {
     if (err) reject(new Error('Cannot load the database'));
+    
     if (data) {
       const fileLines = data.toString('utf-8').trim().split('\n');
       const CS = [];
       const SWE = [];
-      fileLines.forEach(item => {
-        item = item.split(',');
-        if (item[3] === 'CS') CS.push(item[0]);
-        else if (item[3] === 'SWE') SWE.push(item[0]);
+      fileLines.forEach((item) => {
+        const splitted = item.split(',');
+        if (splitted[3] === 'CS') CS.push(splitted[0]);
+        else if (splitted[3] === 'SWE') SWE.push(splitted[0]);
       });
       console.log(`Number of students: ${CS.length + SWE.length}`);
       console.log(`Number of students in CS: ${CS.length}. List: ${CS.join(', ')}`);
